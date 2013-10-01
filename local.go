c99b4c422f534a7f6545769b5bc18d0014f12faa
hominlinx <monkeyyuan635@gmail.com>
2013-10-02 00:58:06 +0800

    this is a blog by golang ,

---
+++ local.go
@@ -0,0 +1,42 @@
+// Copyright 2013 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !appengine
+
+// This file implements a stand-alone blog server.
+
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
+
+var (
+	httpAddr     = flag.String("http", "localhost:8080", "HTTP listen address")
+	contentPath  = flag.String("content", "/app/content/", "path to content files")
+	templatePath = flag.String("template", "/app/template/", "path to template files")
+	staticPath   = flag.String("static", "/app/static/", "path to static files")
+)
+
+func main() {
+	flag.Parse()
+	port := os.Getenv("PORT")
+	if port == "" { //run local
+		*contentPath = "content/"
+		*templatePath = "./template/"
+		*staticPath = "./static/"
+		port = "8080"
+	}
+	s, err := NewServer(*contentPath, *templatePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.Handle("/", s)
+	fs := http.FileServer(http.Dir(*staticPath))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
